8600533185fcb27ec6c63e1af4c793c3f1387959
flyon21 <zhogolevpv@gmail.com>
2021-04-24 20:40:45 +0300

    implevented django rest api tutorial section 1-6

---
+++ snippets/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Snippet
+from django.contrib.auth.models import User
+
+class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+    class Meta:
+        model = Snippet
+        fields = ['url','id', 'title','highlight', 'code', 'linenos', 'language', 'style', 'owner']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets', 'url']
---
+++ snippets/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import SnippetViewSet, UserViewSet, api_root
+
+
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet)
+router.register(r'users', UserViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    ]
+
+
+
+
